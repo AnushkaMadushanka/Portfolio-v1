c5b9c31bcdbf817b20b223ab6e683915cc452e09
AnushkaMadushanka <anushkamadushanka1998@gmail.com>
2019-10-18 14:51:37 +0530

    Updates

---
+++ precache-manifest.ccacb6e991160f3f9ee53bd36d36446b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9f14f3e492ab9bdf8c8562698140f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c8dffe83246f939269b",
+    "url": "/static/css/main.2027d40e.chunk.css"
+  },
+  {
+    "revision": "7fa60b0ad2b24ca10306",
+    "url": "/static/js/2.594f3d55.chunk.js"
+  },
+  {
+    "revision": "8c8dffe83246f939269b",
+    "url": "/static/js/main.42d26ba8.chunk.js"
+  },
+  {
+    "revision": "757627992f6047af2376",
+    "url": "/static/js/runtime~main.f2c7a9f7.js"
+  },
+  {
+    "revision": "02287d1701e3bcce8010bd5a061e96da",
+    "url": "/static/media/js.02287d17.png"
+  },
+  {
+    "revision": "93b99beaa165df6e94287da6731d65e1",
+    "url": "/static/media/logo.93b99bea.svg"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/static/media/node.0d8ed365.png"
+  },
+  {
+    "revision": "88ade5c80702e347cb6971dfbfb56aa1",
+    "url": "/static/media/sqlserver.88ade5c8.jpg"
+  },
+  {
+    "revision": "31f97db277a34b3b20d8827b6c307c71",
+    "url": "/static/media/unity.31f97db2.png"
+  }
+]);
