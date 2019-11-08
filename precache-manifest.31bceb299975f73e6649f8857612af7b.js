9e81315bdc7275d212a5d24435798f379d7dc74e
Anushka Madushanka <anushkamadushanka1998@gmail.com>
2019-11-08 18:05:25 +0530

    Updates

---
+++ precache-manifest.31bceb299975f73e6649f8857612af7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183f803a6e96bcec142aa2ef14c2cd56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86137a88b5b201194052",
+    "url": "/static/css/main.e40d7d79.chunk.css"
+  },
+  {
+    "revision": "95c7df7bf9b10711a1ca",
+    "url": "/static/js/2.4320022d.chunk.js"
+  },
+  {
+    "revision": "86137a88b5b201194052",
+    "url": "/static/js/main.6a3d79b8.chunk.js"
+  },
+  {
+    "revision": "757627992f6047af2376",
+    "url": "/static/js/runtime~main.f2c7a9f7.js"
+  },
+  {
+    "revision": "901ed80a5d261a8db4bc40a563952e62",
+    "url": "/static/media/logo.901ed80a.svg"
+  }
+]);
