5fa6008313c7fa4ce1ccbf4a727144d19888b3f9
Anushka Madushanka <anushkamadushanka1998@gmail.com>
2020-01-05 17:11:11 +0530

    Updates

---
+++ precache-manifest.9d014c085cfdc695bf5426c6eee10e15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9cb742d7219ccfa53253febe9b28fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb006b03bf8c370e3a60",
+    "url": "/static/css/main.eb33e75c.chunk.css"
+  },
+  {
+    "revision": "5a175ddb34fc1e1442c6",
+    "url": "/static/js/2.1c2f5852.chunk.js"
+  },
+  {
+    "revision": "bb006b03bf8c370e3a60",
+    "url": "/static/js/main.524585db.chunk.js"
+  },
+  {
+    "revision": "757627992f6047af2376",
+    "url": "/static/js/runtime~main.f2c7a9f7.js"
+  },
+  {
+    "revision": "901ed80a5d261a8db4bc40a563952e62",
+    "url": "/static/media/logo.901ed80a.svg"
+  }
+]);
